e1ef1d0f4946cbcde726c6fe53ee27c560ced15e
satyajit <satyajitdash61@gmail.com>
2024-10-24 02:38:47 +0530

    jdbc

--- src/main/java/org/example/ProductCatelogService/models/Category.java
+++ src/main/java/org/example/ProductCatelogService/models/Category.java
@@ -1,6 +1,8 @@
 package org.example.ProductCatelogService.models;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,9 +10,11 @@ import java.util.List;
 
 @Getter
 @Setter
+@Entity
 //@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Category extends BaseModel{
     private String name;
     private String description;
+    @OneToMany(mappedBy = "category")
     private List<Product> products;
 }
--- src/main/java/org/example/ProductCatelogService/models/Product.java
+++ src/main/java/org/example/ProductCatelogService/models/Product.java
@@ -3,15 +3,18 @@ package org.example.ProductCatelogService.models;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
+import jakarta.persistence.*;
 
 @Getter
 @Setter
+@Entity
 //@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Product extends BaseModel{
     private String name;
     private String imageUrl;
     private String description;
     private Double price;
+    @ManyToOne(cascade = CascadeType.ALL)
     private Category category;
 
 }
